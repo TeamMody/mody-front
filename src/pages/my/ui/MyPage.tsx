5e2a267498d7712d6193aeb20f765092d9479892
Tnalxmsk <alstnwkd990@naver.com>
2024-12-31 02:30:36 +0900

    :art: style: #8 바텀네비게이션 컴포넌트 ui 추가

--- src/pages/my/ui/MyPage.tsx
+++ src/pages/my/ui/MyPage.tsx
@@ -1,7 +1,22 @@
+import styled from 'styled-components';
+import BottomNavigation from '../../../shared/ui/BottomNavigation.tsx';
+
 export const MyPage = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>MyPage</h1>
-    </div>
+      <BottomNavigation />
+    </Wrapper>
   );
 };
+
+
+const Wrapper = styled.div`
+  display: flex;;
+  flex-direction: column;
+  width: 100vw;
+  max-width: 440px;
+  height: 100vh;
+
+  background-color: ${({ theme }) => theme.colors.gray900};
+`;
