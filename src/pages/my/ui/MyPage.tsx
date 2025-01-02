df5f4b1bc21a4d92e8dced797ffdc0fcd5874869
정휘준 <jhj030811@gmail.com>
2025-01-02 12:41:09 +0900

    :art: style: #13 Divider 컴포넌트 구현

--- src/pages/my/ui/MyPage.tsx
+++ src/pages/my/ui/MyPage.tsx
@@ -5,7 +5,7 @@ import { HeaderAction } from '../../../shared/types';
 import logo from '../../../shared/assets/icon/ic-logo.svg';
 import plus from '../../../shared/assets/icon/ic-plus.svg';
 import hamburger from '../../../shared/assets/icon/ic-hamburger.svg';
-import Divider from '../../../shared/ui/CustomDivider.tsx';
+
 export const MyPage = () => {
   const leftHeaderAction: HeaderAction = { icon: logo, onClick: () => console.log('') };
   const rightHeaderActionArr: HeaderAction[] = [
@@ -21,8 +21,9 @@ export const MyPage = () => {
   );
 };
 
+
 const Wrapper = styled.div`
-  display: flex;
+  display: flex;;
   flex-direction: column;
   width: 100vw;
   max-width: 440px;
