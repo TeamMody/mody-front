0369f05a66b3255d4bf6fd3b5633401cea5f7c61
BitedRadish <a980831@naver.com>
2025-01-02 16:37:15 +0900

    :bug: icon 절대 경로 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,10 @@ export default defineConfig({
         find: '@shared',
         replacement: path.resolve(__dirname, 'src/shared'),
       },
+      {
+        find: '@icon',
+        replacement: path.resolve(__dirname, 'src/shared/assets/icon'),
+      },
     ],
   },
 });
