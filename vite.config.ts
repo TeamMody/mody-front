00541667942b8ae1a9b5ae4cb3974a8ed4f2e6d0
BitedRadish <a980831@naver.com>
2025-01-06 18:48:35 +0900

    :art: style: 비슷 페이지 공통 컴포넌트 (progressBar 및 게시글 컴포넌트 UI) 구현

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.jpeg', '**/*.gif'], // 이미지 파일 포함
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
