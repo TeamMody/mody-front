5e80a499ee097f670b7545ff56c4c5a9bdc320f9
BitedRadish <a980831@naver.com>
2025-01-06 18:48:35 +0900

    :art: style: 비슷 페이지 공통 컴포넌트 (progressBar 및 게시글 컴포넌트 UI) 구현

--- src/pages/post/ui/PostPage.tsx
+++ src/pages/post/ui/PostPage.tsx
@@ -1,7 +1,28 @@
 import styled from 'styled-components';
-import AppBar from '../../../shared/ui/AppBar.tsx';
-import logo from '../../../shared/assets/icon/ic-logo.svg';
-import plus from '../../../shared/assets/icon/ic-plus.svg';
+import AppBar from '@shared/ui/AppBar.tsx';
+import logo from '@shared/assets/icon/ic-logo.svg';
+import plus from '@shared/assets/icon/ic-plus.svg';
+import Post from '@shared/ui/Post.tsx';
+import TempImg1 from '@shared/assets/images/tempImg1.jpg';
+import TempImg2 from '@shared/assets/images/tempImg2.jpg';
+
+interface PostPropsType {
+  images: string[];
+  name: string;
+  type: string;
+  description: string;
+  likeCount: number;
+  isLiked: boolean;
+}
+const mockData: PostPropsType = {
+  images: [TempImg1, TempImg2],
+  name: '사람1',
+  type: '네모형 체형',
+  description:
+    '인녕하세요 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ',
+  likeCount: 112,
+  isLiked: true,
+};
 
 export const PostPage = () => {
   const leftHeaderAction = { icon: logo, onClick: () => console.log('') };
@@ -10,6 +31,17 @@ export const PostPage = () => {
   return (
     <>
       <AppBar leftHeaderAction={leftHeaderAction} rightHeaderActionArr={rightHeaderActionArr} />
+
+      <Main>
+        <Post data={mockData} />
+      </Main>
     </>
   );
 };
+
+const Main = styled.main`
+  width: 100%;
+  height: 79%;
+  border: 1px solid green;
+  padding: 16px 20px;
+`;
--- src/shared/ui/Post.tsx
+++ src/shared/ui/Post.tsx
@@ -1,11 +1,141 @@
 import styled from 'styled-components';
+import ProgressBar from '@shared/ui/ProgressBar';
+import { useState } from 'react';
+import Heart from '@shared/assets/icon/ic-heart.svg?react';
+import MoreVertical from '@shared/assets/icon/ic-more-vertical.svg?react';
 
-const Post = () => {
-  return <Container>Post</Container>;
+interface PostPropsType {
+  data: {
+    images: string[];
+    name: string;
+    type: string;
+    description: string;
+    likeCount: number;
+    isLiked: boolean;
+  };
+}
+
+const Post = ({ data }: PostPropsType) => {
+  const [imgIdx, setImgIdx] = useState<number>(0);
+  return (
+    <PostContainer bgImage={data.images[imgIdx]}>
+      <ProgressBar length={data.images.length} curIdx={imgIdx}></ProgressBar>
+      <InfoContainer>
+        <UserInfo>
+          <span className="user-name">{data.name}</span>
+          <span className="user-type">{data.type}</span>
+        </UserInfo>
+        <DescriptionContainer>
+          <p className="description">
+            {data.description}
+            <span className="more-btn">더보기</span>
+          </p>
+          <IconContainer>
+            <div className="heart">
+              <Heart></Heart>
+              <span>{data.likeCount}</span>
+            </div>
+            <div className="more-vertical">
+              <MoreVertical></MoreVertical>
+            </div>
+          </IconContainer>
+        </DescriptionContainer>
+      </InfoContainer>
+    </PostContainer>
+  );
 };
 
-const Container = styled.div`
-  width: 100vw;
-  height: 80vh;
+const PostContainer = styled.div<{ bgImage: string }>`
+  width: 100%;
+  height: 100%;
+  border: 1px solid white;
+  position: relative;
+  background-color: ${({ theme }) => theme.colors.gray600};
+  background-image: url(${({ bgImage }) => bgImage});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  border-radius: 10px;
 `;
+
+const InfoContainer = styled.div`
+  padding: 0px 15px 24px 15px;
+  background-color: transparent;
+
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+  height: 20%;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  & > .user-name {
+    font-size: ${({ theme }) => theme.fonts.heading_bold_24px};
+  }
+  & > .user-type {
+    font-size: ${({ theme }) => theme.fonts.heading_medium_18px};
+  }
+`;
+
+const DescriptionContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  .description {
+    position: relative;
+    width: 70%;
+    height: 60%;
+    line-height: 90%;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-word;
+
+    .more-btn {
+      position: absolute;
+      white-space: nowrap;
+      text-align: center;
+      margin-bottom: auto;
+      bottom: -3px;
+      right: 0;
+      font-size: ${({ theme }) => theme.fonts.detail_medium_12px};
+    }
+  }
+`;
+// p태그 가운데 정렬 px단위 말고 해결할 수 있나 ?
+
+const IconContainer = styled.div`
+  width: 20%;
+  height: 100%;
+  display: flex;
+
+  div {
+    width: 50%;
+  }
+  .heart {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3px;
+
+    span {
+      font-size: ${({ theme }) => theme.fonts.detail_medium_12px};
+    }
+  }
+
+  .more-vertical {
+    text-align: center;
+  }
+`;
+
 export default Post;
