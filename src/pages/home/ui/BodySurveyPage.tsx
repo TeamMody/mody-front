22dcb2d74378047ef07c59fa98bfdfdd788f3d78
Tnalxmsk <alstnwkd990@naver.com>
2025-01-07 15:12:40 +0900

    :sparkles: feat: #30 answers 초기화 로직 구현

--- src/pages/home/ui/BodySurveyPage.tsx
+++ src/pages/home/ui/BodySurveyPage.tsx
@@ -2,25 +2,46 @@ import styled from 'styled-components';
 import AppBar from '@shared/ui/AppBar.tsx';
 import IcLeftArrow from '@shared/assets/icon/ic-left-arrow.svg';
 import { useNavigate } from 'react-router';
-import BottomNavigation from '@shared/ui/BottomNavigation.tsx';
 import Question from '@home/components/Question.tsx';
 import { surveyList } from '@shared/apis/home/mocks.ts';
+import { useAnswersStore } from '@home/feature/store/useAnswersStore.ts';
+import CustomButton from '@shared/ui/CustomButton.tsx';
+import { useEffect } from 'react';
 
 export const BodySurveyPage = () => {
   const navigate = useNavigate();
   const leftHeaderAction = { icon: IcLeftArrow, onClick: () => navigate(-1) };
+  const { myAnswers } = useAnswersStore();
+
+  useEffect(() => {
+    useAnswersStore.setState({ myAnswers: Array.from({ length: surveyList.length }, () => '') });
+  }, []);
 
   return (
     <Wrapper>
       <AppBar leftHeaderAction={leftHeaderAction} title="체형 타입 분석" />
       <Container>
         <QuestionsContainer>
-          {surveyList.map((survey) => (
-            <Question key={survey.id} {...survey} />
+          {surveyList.map((survey, index) => (
+            <Question
+              key={survey.id}
+              index={index}
+              {...survey}
+            />
           ))}
+          {myAnswers.filter((myAnswer) => myAnswer !== '').length === surveyList.length
+            ? <CustomButton
+              label="체형 분석하기"
+              onClick={() => console.log('체형 분석')}
+              active={true}
+              paddingTop="16px"
+              paddingBottom="16px"
+              marginHorizontal='20px'
+            />
+            : null
+          }
         </QuestionsContainer>
       </Container>
-      <BottomNavigation />
     </Wrapper>
   );
 };
@@ -37,6 +58,8 @@ const Wrapper = styled.div`
 const Container = styled.div`
   padding-top: 20px;
   overflow-y: scroll;
+  height: 100%;
+  align-content: center;
 `;
 
 
@@ -44,4 +67,5 @@ const QuestionsContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 30px;
+  padding-bottom: 3vh;
 `;
