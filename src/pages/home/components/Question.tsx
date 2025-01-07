98d29d69adb84977b6e7cd245b85ed3170b80927
Tnalxmsk <alstnwkd990@naver.com>
2025-01-07 15:13:26 +0900

    :sparkles: feat: #30 답변 visible 로직 구현

--- src/pages/home/components/Question.tsx
+++ src/pages/home/components/Question.tsx
@@ -1,19 +1,38 @@
 import { BodyTypeAnswer } from '@shared/types';
 import styled from 'styled-components';
 import Answer from '@home/components/Answer.tsx';
+import { useAnswersStore } from '@home/feature/store/useAnswersStore.ts';
 
 interface QuestionProps {
   question: string;
   answers: BodyTypeAnswer[];
+  index: number;
 }
 
-const Question = ({ question, answers }: QuestionProps) => {
+const Question = ({ question, answers, index }: QuestionProps) => {
+  const { myAnswers } = useAnswersStore();
+
+  if (index !== 0) {
+    if (myAnswers[index - 1] === '') {
+      return null;
+    }
+  }
+
+  if (myAnswers[index] !== '') {
+    return (
+      <Container>
+        <QuestionText>{question}</QuestionText>
+        <SelectedAnswer>{myAnswers[index]}</SelectedAnswer>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <QuestionText>{question}</QuestionText>
       <AnswersContainer>
         {answers.map((answer) => (
-          <Answer key={answer.id} {...answer} />
+          <Answer key={answer.id} index={index} {...answer} />
         ))}
       </AnswersContainer>
     </Container>
@@ -38,4 +57,13 @@ const AnswersContainer = styled.div`
   gap: 16px;
   scroll-snap-type: x mandatory;
   padding: 0 59px;
+`;
+
+const SelectedAnswer = styled.div`
+  font: ${({ theme }) => theme.fonts.body_medium_16px};
+  padding: 10px 16px;
+  border: none;
+  border-radius: 10px;
+  margin: 18px 20px 0 20px;
+  background-color: ${({ theme }) => theme.colors.gray600};
 `;
