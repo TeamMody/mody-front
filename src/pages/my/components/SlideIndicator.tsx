be1514e5de2c026f619a8bd49f7462ec3cfc7dc6
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-01-09 00:29:11 +0900

    :art: style: #39 중앙 탭바 Slide Indicator 위치 수정

--- src/pages/my/components/SlideIndicator.tsx
+++ src/pages/my/components/SlideIndicator.tsx
@@ -6,8 +6,8 @@ const SlideIndicator = ({ activeTabIndex }: { activeTabIndex: number }) => {
 const IndicatorWrapper = styled.div<{ $activeTabIndex: number }>`
   position: absolute;
   bottom: 0;
-  left: ${({ $activeTabIndex }) => `calc(${$activeTabIndex * 33.5 + 9.5}%)`};
-  width: 14%;
+  left: ${({ $activeTabIndex }) => `calc(${$activeTabIndex * 33.4 + 10.1}%)`};
+  width: 12.8%;
   height: 3px;
   background-color: ${({ theme }) => theme.colors.green500};
   transition: left 0.3s ease-out;
