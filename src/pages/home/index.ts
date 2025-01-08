33919cebdb340305f7af5f6154129a8904b09301
Tnalxmsk <alstnwkd990@naver.com>
2025-01-08 14:47:46 +0900

    :sparkles: feat: #37 로딩 화면 구현

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,2 +1,4 @@
 export { HomePage } from '@home/ui/HomePage';
 export { BodySurveyPage } from '@home/ui/BodySurveyPage';
+export { LoadingPage } from '@home/ui/LoadingPage';
+export { BodyTypePage } from '@home/ui/BodyTypePage';
