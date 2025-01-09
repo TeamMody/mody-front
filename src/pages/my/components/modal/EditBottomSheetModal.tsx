17297e751b744c44f8bf6c7dcb87a62882a1b150
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-01-09 15:26:53 +0900

    :art: style: #39 styled components를 활용하여 Bottom Sheet CSS 수정

--- src/pages/my/components/modal/EditBottomSheetModal.tsx
+++ src/pages/my/components/modal/EditBottomSheetModal.tsx
@@ -12,34 +12,31 @@ interface EditBottomSheetProps {
 
 const EditBottomSheet = ({ isOpen, onClose }: EditBottomSheetProps) => {
   const ref = useRef<SheetRef>(null);
+
   return (
-    <>
-      <CustomSheet ref={ref} isOpen={isOpen} onClose={onClose}>
-        <Sheet.Container>
-          <Sheet.Content>
-            <BottomSheetItem content="수정하기" icon={IcPen} />
-            <BottomSheetItem content="삭제하기" icon={IcTrashCan} />
-            <BottomSheetItem content="나만보기" />
-          </Sheet.Content>
-        </Sheet.Container>
-      </CustomSheet>
-    </>
+    <Sheet isOpen={isOpen} onClose={onClose} ref={ref}>
+      <SheetContainer>
+        <SheetContent>
+          <BottomSheetItem content="수정하기" icon={IcPen} />
+          <BottomSheetItem content="삭제하기" icon={IcTrashCan} />
+          <BottomSheetItem content="나만보기" />
+        </SheetContent>
+      </SheetContainer>
+    </Sheet>
   );
 };
 
-const CustomSheet = styled(Sheet)`
-  .react-modal-sheet-container {
-    height: 22.3vh !important;
-    width: 100vw !important;
-    border-radius: 30px 30px 0 0 !important;
-    background-color: ${({ theme }) => theme.colors.gray800} !important;
-  }
+const SheetContainer = styled(Sheet.Container)`
+  height: 22.3vh !important;
+  width: 100vw !important;
+  border-radius: 30px 30px 0 0 !important;
+  background-color: ${({ theme }) => theme.colors.gray800} !important;
+`;
 
-  .react-modal-sheet-content {
-    padding: 4.1vh 5.1vw 0 5.1vw;
-    display: flex;
-    flex-direction: column;
-  }
+const SheetContent = styled(Sheet.Content)`
+  padding: 4.1vh 5.1vw 0 5.1vw;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default EditBottomSheet;
