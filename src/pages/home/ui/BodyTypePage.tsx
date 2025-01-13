c1e805d59da2b0c38227893bc3b4916b8fb7edc4
Tnalxmsk <alstnwkd990@naver.com>
2025-01-13 16:41:41 +0900

    :recycle: refactor: 체형타입 결과 컴포넌트 분리

--- src/pages/home/ui/BodyTypePage.tsx
+++ src/pages/home/ui/BodyTypePage.tsx
@@ -1,15 +1,8 @@
 import styled from 'styled-components';
-import IcLogo from '@shared/assets/icon/ic-logo.svg';
-import IcBodyTypeSt from '@shared/assets/icon/ic-body-type-st.svg';
-import CustomDivider from '@shared/ui/CustomDivider.tsx';
 import CustomButton from '@shared/ui/CustomButton.tsx';
 import { useNavigate } from 'react-router';
-
-const detail = '이름 님의 체형은 스트레이트형에 가깝습니다. 골격이 상대적으로 근육감이 느껴지는 어깨와 탄탄한 허벅지를 가진 반면, 허리의 굴곡은 크지 않고 엉덩이는 입체감이 적습니다. 목이 길고 쇄골이 눈에 띄지 않으며, 다리 길이가 상체에 비해 약간 짧은 편입니다. 이 체형은 상체와 하체의 균형을 고려한 스타일링이 중요하며, 체형적 장점을 돋보이게 하기 위해 적절한 실루엣 선택이 필요합니다.';
-
-const emphasize = '어깨와 허벅지의 근육감 있는 라인. 이 부분은 피트감이 있는 상의나 슬림 핏 팬츠를 활용해 강조하면 좋습니다. 상체의 볼륨감이 자연스러운 매력을 더해줄 수 있습니다. ';
-
-const supplementation = '허리가 굴곡이 뚜렷하지 않으므로, 시각적으로 허리를 강조할 수 있는 스타일링이 필요합니다. 다리 길이를 더 길어 보이게 연출하는 하이웨스트 디자인을 추천합니다.';
+import BodyTypeContent from '@home/components/BodyTypeContent.tsx';
+import { bodyTypeResult } from '@shared/apis/home/mocks.ts';
 
 export const BodyTypePage = () => {
   const navigate = useNavigate();
@@ -20,23 +13,7 @@ export const BodyTypePage = () => {
 
   return (
     <Wrapper>
-      <BodyTypeContainer>
-        <Logo src={IcLogo} />
-        <BodyTypeImage src={IcBodyTypeSt} />
-        <BodyType>스트레이트 타입</BodyType>
-      </BodyTypeContainer>
-      <DescriptionContainer>
-        <CustomDivider width="100%" border="0.5px" />
-        <Detail>
-          {detail}
-        </Detail>
-        <CustomDivider width="100%" border="0.5px" />
-        <Bold>강조할 부분</Bold>
-        <Detail>{emphasize}</Detail>
-        <CustomDivider width="100%" border="0.5px" />
-        <Bold>보완할 부분</Bold>
-        <Detail>{supplementation}</Detail>
-      </DescriptionContainer>
+      <BodyTypeContent {...bodyTypeResult} />
       <ButtonContainer>
         <CustomButton label="완료" onClick={handleNavigate} active={true} paddingTop="19px" paddingBottom="19px" />
       </ButtonContainer>
@@ -53,46 +30,6 @@ const Wrapper = styled.div`
   padding: 0 20px;
   background-color: ${({ theme }) => theme.colors.gray900};
   overflow-y: scroll;
-`;
-
-const BodyTypeContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Logo = styled.img`
-  object-fit: cover;
-  width: 26.7%;
-  margin-top: 39px;
-`;
-
-const BodyTypeImage = styled.img`
-  object-fit: cover;
-  margin-top: 31px;
-  margin-bottom: 25px;
-`;
-
-const BodyType = styled.p`
-  font: ${({ theme }) => theme.fonts.heading_bold_24px};
-  color: ${({ theme }) => theme.colors.green500};
-`;
-
-const DescriptionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 17px;
-  gap: 24px;
-`;
-
-const Bold = styled.p`
-  font: ${({ theme }) => theme.fonts.heading_medium_20px};
-  font-weight: bold;
-`;
-
-const Detail = styled.p`
-  font: ${({ theme }) => theme.fonts.body_medium_16px};
 `;
 
 const ButtonContainer = styled.div`
