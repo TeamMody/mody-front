3de55a334d1e6cec4ea25de75815e68e7a6a548d
BitedRadish <a980831@naver.com>
2025-01-17 16:20:40 +0900

    :sparkles: feat: axios 인스턴스 수정

--- src/shared/apis/instance/index.ts
+++ src/shared/apis/instance/index.ts
@@ -1,27 +1,31 @@
 import axios from 'axios';
 
 //auth
-
 export const authApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/auth`,
+  timeout: 5000,
 });
 
 //Oauth
 export const OAuthApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/oauth2`,
+  timeout: 5000,
 });
 
 //style-analysis
 export const styleAnalysisApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/style-analysis`,
+  timeout: 5000,
 });
 
 //body-analysis
 export const bodyAnalysisApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/body-analysis`,
+  timeout: 5000,
 });
 
 //posts
 export const postsApi = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/posts`,
+  timeout: 5000,
 });
