a1c127955e059dcf250aad319d68bcd6d2bfcdcf
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-01-22 16:55:46 +0900

    :art: style: navigate 추가

--- src/pages/onboarding/ui/OnboardingPage.tsx
+++ src/pages/onboarding/ui/OnboardingPage.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import Logo from '@shared/assets/icon/ic-onboarding-logo.svg?react';
 import KakaoLogo from '@pages/onboarding/icons/ic-kakao-logo.svg?react';
+import { useNavigate } from 'react-router';
 
 export const OnboardingPage = () => {
+  const navigate = useNavigate();
   // const handleAccount = (e) => {
   //   console.log(e);
   // };
