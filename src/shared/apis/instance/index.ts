cbfc872d226784659fd5ad7020b8370f9db8a156
BitedRadish <a980831@naver.com>
2025-01-22 16:09:28 +0900

    :bug: fix: axios instance 수정

--- src/shared/apis/instance/index.ts
+++ src/shared/apis/instance/index.ts
@@ -2,30 +2,6 @@ import axios from 'axios';
 
 //auth
 export const authApi = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/auth`,
-  timeout: 5000,
-});
-
-//Oauth
-export const OAuthApi = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/oauth2`,
-  timeout: 5000,
-});
-
-//style-analysis
-export const styleAnalysisApi = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/style-analysis`,
-  timeout: 5000,
-});
-
-//body-analysis
-export const bodyAnalysisApi = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/body-analysis`,
-  timeout: 5000,
-});
-
-//posts
-export const postsApi = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}/posts`,
+  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}`,
   timeout: 5000,
 });
