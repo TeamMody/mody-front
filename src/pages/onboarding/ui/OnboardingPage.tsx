f8d2769f5be67659652590f5e3b2102bf51120a7
BitedRadish <a980831@naver.com>
2025-01-25 01:24:12 +0900

    :sparkles: feat: axios 인터셉터 임시 설정 1. 로그인 컴포넌트 구현 후 api 응답 값 보고 수정 예정

--- src/pages/onboarding/ui/OnboardingPage.tsx
+++ src/pages/onboarding/ui/OnboardingPage.tsx
@@ -3,7 +3,7 @@ import Logo from '@shared/assets/icon/ic-onboarding-logo.svg?react';
 import KakaoLogo from '@pages/onboarding/icons/ic-kakao-logo.svg?react';
 import { useNavigate } from 'react-router';
 import axios from 'axios';
-
+import { apiInstance } from '@shared/apis/instance';
 import { useEffect } from 'react';
 
 export const OnboardingPage = () => {
@@ -18,8 +18,16 @@ export const OnboardingPage = () => {
 
   useEffect(() => {
     const test = () => {
-      axios.get();
+      apiInstance
+        .get('/test')
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     };
+    test();
   }, []);
   return (
     <Wrapper>
--- src/shared/store/token.ts
+++ src/shared/store/token.ts
@@ -6,11 +6,11 @@ interface TokenState {
   logOut: () => void;
 }
 
-const useTokenStore = create<TokenState>((set) => ({
+const useAuthStore = create<TokenState>((set) => ({
   accessToken:
     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1IiwiaWF0IjoxNzM3NzI5MDA4LCJleHAiOjE3Mzc3NjUwMDh9.XkttCOBJ1z9CZbPGIsmyh4SbvYZFS0s4jHYMPRM-i_A ',
   setAccessToken: (token: string) => set({ accessToken: token }),
   logOut: () => set({ accessToken: '' }),
 }));
 
-export default useTokenStore;
+export default useAuthStore;
