b6ef205f0df3fc25c902d20a3b6d381eb310266e
BitedRadish <a980831@naver.com>
2025-01-25 03:57:12 +0900

    :sparkles: feat: SignInPage UI 구현 1. api 연결 필요

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -11,6 +11,7 @@ import { PostPage } from '@pages/post';
 import { MyPage } from '@pages/my';
 import { OnboardingPage, InputUser } from '@pages/onboarding';
 import SignUpPage from '@pages/onboarding/ui/SignUpMain';
+import SignInPage from '@pages/onboarding/pages/SignInPage';
 
 import { CreatePost } from '@pages/post/components/CreatePost';
 export const router = createBrowserRouter([
@@ -56,6 +57,10 @@ export const router = createBrowserRouter([
       {
         path: 'signup',
         element: <SignUpPage />,
+      },
+      {
+        path: 'signin',
+        element: <SignInPage />,
       },
     ],
   },
