d4ef347936e2f04bfbf0e5f1bd8b24c0cf6778d3
Tnalxmsk <alstnwkd990@naver.com>
2025-01-30 15:22:33 +0900

    :sparkles: feat: 유저 정보 fetch 기능 구현

--- src/shared/apis/member/member.ts
+++ src/shared/apis/member/member.ts
@@ -3,8 +3,7 @@ import { BaseResponse, MemberInfo } from '@shared/types';
 
 export const fetchMyInfo = async (): Promise<BaseResponse<MemberInfo> | undefined> => {
   try {
-    const response = await apiInstance.get<BaseResponse<MemberInfo>>('/members/me',
-      { headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5IiwiaWF0IjoxNzM4MTQyMzI1LCJleHAiOjE3MzgxNzgzMjV9.gPB87dpoLwPou6TAnL3_Ry3zU_VklLlKyB4eHV8VBcg` } });
+    const response = await apiInstance.get<BaseResponse<MemberInfo>>('/members/me')
     console.log(response.data);
     return response.data;
   } catch ( error ) {
