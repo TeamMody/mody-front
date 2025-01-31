c5dfec6eadcfe8ea790396b715f983800e53d671
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-01-31 17:39:02 +0900

    :fire: remove: #92 accessToken 제거

--- src/shared/store/token.ts
+++ src/shared/store/token.ts
@@ -7,8 +7,7 @@ interface TokenState {
 }
 
 const useAuthStore = create<TokenState>((set) => ({
-  accessToken:
-    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwiaWF0IjoxNzM4MzA3OTE5LCJleHAiOjE3MzgzNDM5MTl9.k59GoxoGctj8fFzKUXAUJt0A4raeg6zPrArGAjneFWg',
+  accessToken: '',
   setAccessToken: (token: string) => set({ accessToken: token }),
   logOut: () => set({ accessToken: '' }),
 }));
