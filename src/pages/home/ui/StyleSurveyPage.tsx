d9d3a7dd530154a17ffe0b38f3ed22b53a8a31d5
Tnalxmsk <alstnwkd990@naver.com>
2025-02-04 22:16:54 +0900

    :sparkles: feat: 스타일 카테고리 API 연결

--- src/pages/home/ui/StyleSurveyPage.tsx
+++ src/pages/home/ui/StyleSurveyPage.tsx
@@ -5,13 +5,14 @@ import IcLeftArrow from '@icon/ic-left-arrow.svg';
 import { useLocation, useNavigate } from 'react-router';
 import StyleSurvey from '@home/components/StyleSurvey.tsx';
 import CustomButton from '@shared/ui/CustomButton.tsx';
-import { keywords, styleKeywords } from '@shared/apis/home/mocks.ts';
 import { useStyleSurveyStore } from '@home/feature/store/useStyleSurveyStore.ts';
 import { useCallback, useEffect } from 'react';
 import { useMyInfoStore } from '@shared/store/useMyInfoStore.ts';
-import { Loading } from '@home/components/Loading.tsx';
+import { RecommendationLoading } from '@home/components/RecommendationLoading.tsx';
+import { Loading } from '@shared/ui/Loading.tsx';
 import debounce from 'lodash/debounce';
 import { usePostRecommendations } from '@home/feature/hooks/mutate/usePostRecommendations.ts';
+import { useGetStyleCategories } from '@home/feature/hooks/query/useGetStyleCategories.ts';
 
 export const StyleSurveyPage = () => {
   const { myInfo } = useMyInfoStore();
@@ -19,6 +20,7 @@ export const StyleSurveyPage = () => {
   const { type } = useLocation().state as { type: RecommendationType };
   const navigate = useNavigate();
   const { mutate, isSuccess, data, isPending } = usePostRecommendations();
+  const { data: categories, isPending: isCategoriesPending, isError: isCategoriesError } = useGetStyleCategories();
 
   useEffect(() => {
     resetKeywords();
@@ -47,21 +49,27 @@ export const StyleSurveyPage = () => {
   }, [isSuccess]);
 
   if (isPending) {
-    return <Loading type={type} />;
+    return <RecommendationLoading type={type} />;
+  }
+
+  if (isCategoriesError) {
+    return <div>에러가 발생했습니다.</div>;
   }
 
   return (
     <Wrapper>
       <AppBar leftHeaderAction={leftHeaderAction} title={type} />
-      <KeywordsContainer>
-        <StyleSurvey category="liked" keywords={keywords} />
-        <StyleSurvey category="disliked" keywords={keywords} />
-        <StyleSurvey category="image" keywords={styleKeywords} />
-        <ButtonContainer>
-          <CustomButton label="스타일 추천 받기" onClick={debouncedApiRequest} active={true} paddingTop="19px"
-                        paddingBottom="19px" />
-        </ButtonContainer>
-      </KeywordsContainer>
+      {!isCategoriesPending ? <KeywordsContainer>
+          <StyleSurvey category="liked" keywords={categories?.result.styleCategories!} />
+          <StyleSurvey category="disliked" keywords={categories?.result.styleCategories!} />
+          <StyleSurvey category="image" keywords={categories?.result.appealCategories!} />
+          <ButtonContainer>
+            <CustomButton label="스타일 추천 받기" onClick={debouncedApiRequest} active={true} paddingTop="19px"
+                          paddingBottom="19px" />
+          </ButtonContainer>
+        </KeywordsContainer>
+        : <Loading />
+      }
     </Wrapper>
   );
 };
