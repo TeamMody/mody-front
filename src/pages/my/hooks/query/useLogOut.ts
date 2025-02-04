6391054e7eb052d95f5f50d044ea371671aaed0f
정휘준 <jhj030811@gmail.com>
2025-02-05 01:24:07 +0900

    :sparkels/#120 로그아웃 api 연결중

--- src/pages/my/hooks/query/useLogOut.ts
+++ src/pages/my/hooks/query/useLogOut.ts
@@ -1,3 +1,7 @@
-export const useLogOut = () => {
-  console.log('하이');
+import { apiInstance } from '@shared/apis/instance';
+
+export const useLogOut = async () => {
+  const res = await apiInstance.post('/auth/logout', {}, { withCredentials: true });
+  console.log(res);
+  console.log('로그아웃 성공');
 };
