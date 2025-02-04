6fa3fc3ba0705cc81a783ffc2a6b7d0ae60151ab
Tnalxmsk <alstnwkd990@naver.com>
2025-02-04 22:15:40 +0900

    :sparkles: feat: 스타일 카테고리 response type 정의

--- src/shared/apis/recommendations/recommendations.ts
+++ src/shared/apis/recommendations/recommendations.ts
@@ -3,36 +3,35 @@ import {
   RecommendationType,
   RecommendationRequest,
   RecommendationResponse,
-  LikeResponse,
+  LikeResponse, StyleKeywordResponse,
 } from '@shared/types';
 import { apiInstance } from '@shared/apis/instance';
 
 export const fetchPostRecommendations = async (data: RecommendationRequest, type: RecommendationType): Promise<BaseResponse<RecommendationResponse> | undefined> => {
   try {
-    const url = type === RecommendationType.STYLE ? 'style-analysis' : 'fashion-item-analysis'
+    const url = type === RecommendationType.STYLE ? 'style-analysis' : 'fashion-item-analysis';
     const response = await apiInstance.post<BaseResponse<RecommendationResponse>>(`/recommendations/${url}`, data, {
-        timeout: 10000,
-      });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-export const fetchStyleCategories = async () => {
-  try {
-    const response = await apiInstance.get('/style-analysis')
-    console.log(response.data)
+      timeout: 10000,
+    });
     return response.data;
   } catch ( error ) {
-    console.error( error );
+    console.error(error);
   }
-}
+};
+
+export const fetchStyleCategories = async (): Promise<BaseResponse<StyleKeywordResponse> | undefined> => {
+  try {
+    const response = await apiInstance.get<BaseResponse<StyleKeywordResponse>>('/recommendations/categories');
+    console.log(response.data);
+    return response.data;
+  } catch ( error ) {
+    console.error(error);
+  }
+};
 
 export const fetchRecommendationLike = async (id: number): Promise<BaseResponse<LikeResponse> | undefined> => {
   try {
     const response = await apiInstance.post<BaseResponse<LikeResponse>>(`/recommendations/${id}/like`);
-    console.log(response.data);
     return response.data;
   } catch ( error ) {
     console.error(error);
