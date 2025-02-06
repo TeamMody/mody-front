602fda102e2c75ac5dc8183013e040c2a2dc2b2a
정휘준 <jhj030811@gmail.com>
2025-02-07 02:16:21 +0900

    로그인 실험중

--- src/shared/store/token.ts
+++ src/shared/store/token.ts
@@ -7,11 +7,12 @@ interface TokenState {
   logOut: () => void;
 }
 
-/*const useAuthStore = create<TokenState>((set) => ({
-  accessToken: '',
-  setAccessToken: (token: string) => set({ accessToken: token }),
-  logOut: () => set({ accessToken: '' }),
-}));*/
+// export const useAuthStore = create<TokenState>((set) => ({
+//   accessToken:
+//     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNyIsImlhdCI6MTczODg2MTU5NSwiZXhwIjoxNzM4ODk3NTk1fQ.9FEeU7wwDNv6PoHMnO0Ti9IZKu5wteMS6lWA1sW_GMw',
+//   setAccessToken: (token: string) => set({ accessToken: token }),
+//   logOut: () => set({ accessToken: '' }),
+// }));
 
 export const useAuthStore = create<TokenState>()(
   persist(
