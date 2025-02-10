7a0ca4e7fe057365d0f33776e30db0bbea9d5807
Tnalxmsk <alstnwkd990@naver.com>
2025-02-10 16:12:43 +0900

    :recycle: refactor: query key 수정

--- src/pages/my/hooks/mutate/useGetBodyTypeResult.ts
+++ src/pages/my/hooks/mutate/useGetBodyTypeResult.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchBodyAnalysisResult } from '@shared/apis/body/body.ts';
 
-export const useGetBodyTypeResult = () => {
+export const useGetBodyTypeResult = (id: number) => {
   const result = useQuery({
-    queryKey: ['bodyTypeResult'],
+    queryKey: ['bodyTypeResult', id],
     queryFn: () => fetchBodyAnalysisResult(),
   })
 
