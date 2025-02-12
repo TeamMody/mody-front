d0faefe7cc998d4ec5e55eb01454624213e9c51d
정휘준 <jhj030811@gmail.com>
2025-02-12 15:53:22 +0900

    병합중

--- src/shared/store/token.ts
+++ src/shared/store/token.ts
@@ -6,13 +6,6 @@ interface TokenState {
   logOut: () => void;
 }
 
-// export const useAuthStore = create<TokenState>((set) => ({
-//   accessToken:
-//     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNyIsImlhdCI6MTczODg2MTU5NSwiZXhwIjoxNzM4ODk3NTk1fQ.9FEeU7wwDNv6PoHMnO0Ti9IZKu5wteMS6lWA1sW_GMw',
-//   setAccessToken: (token: string) => set({ accessToken: token }),
-//   logOut: () => set({ accessToken: '' }),
-// }));
-
 const useAuthStore = create<TokenState>((set) => ({
   accessToken: '',
   setAccessToken: (token: string) => set({ accessToken: token }),
