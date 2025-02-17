3c0b1379559e534eb9ca1766351786706f10b1a2
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-02-17 22:58:28 +0900

    :bug: fix: #148 무한 스크롤 새로고침 문제 해결

--- src/pages/my/components/MyAndLikePosts.tsx
+++ src/pages/my/components/MyAndLikePosts.tsx
@@ -23,7 +23,7 @@ const MyAndLikePosts = ({ activeIndex }: { activeIndex: ActiveIndex }) => {
 
   const { containerRef } = useInfiniteScroll({ hasNextPage, isFetchingNextPage, fetchNextPage }); // 스크롤 감지
 
-  if (isLoading || isFetching) {
+  if (isLoading) {
     return (
       <LoadingWrapper>
         <SmallLoading />
