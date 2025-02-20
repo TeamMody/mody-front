3e88bbf26d51b29a51b461aa7d694d4433275ed1
Tnalxmsk <alstnwkd990@naver.com>
2025-02-20 17:14:11 +0900

    :recycle: refactor: 주석 처리

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,14 +1,13 @@
-import { Outlet, useNavigate } from 'react-router';
+import { Outlet } from 'react-router';
 import BottomNavigation from '@shared/ui/BottomNavigation';
 import styled from 'styled-components';
-import useIsLoggedInStore from '@shared/store/useIsLoggedIn.ts';
 
 export default function RootLayout() {
-  const { isLoggedIn } = useIsLoggedInStore();
+  /*const { isLoggedIn } = useIsLoggedInStore();
   const navigate = useNavigate();
   if (!isLoggedIn) {
     navigate('/');
-  }
+  }*/
 
   return (
     <Wrapper>
