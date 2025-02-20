879a5b861cdfeb1bd14ee678c0435deb314e031d
정휘준 <jhj030811@gmail.com>
2025-02-21 01:49:12 +0900

    :fix/#172 게시글 수정 줌 버튼 활성화 완료

--- src/shared/ui/ImageCarousel.tsx
+++ src/shared/ui/ImageCarousel.tsx
@@ -63,7 +63,7 @@ const StyledCarousel = styled(Carousel)<{
   imgZoomed?: boolean;
 }>`
   width: 100%;
-  height: ${({ imgZoomed, height }) => (imgZoomed ? '53.791vh' : height)};
+  height: ${({ imgZoomed, height }) => (imgZoomed ? '70.791vh' : height)};
   background-color: ${({ theme }) => theme.colors.gray500};
   filter: ${({ isExpanded }) => (isExpanded ? 'brightness(0.5)' : '')};
   div {
@@ -74,6 +74,7 @@ const StyledCarousel = styled(Carousel)<{
 
     img {
       max-width: 100%;
+      height: ${({ imgZoomed, height }) => (imgZoomed ? '53.791vh' : height)};
     }
   }
 `;
