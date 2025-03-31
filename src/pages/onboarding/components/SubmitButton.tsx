938e1f7613d8616c61e1b2d69dc50eeea7168ca4
Tnalxmsk <alstnwkd990@naver.com>
2025-04-01 01:02:01 +0900

    :recycle: refactor: 제출 버튼 컴포넌트 props 추가
    isLoading prop을 통한 버튼 disabled 처리

--- src/pages/onboarding/components/SubmitButton.tsx
+++ src/pages/onboarding/components/SubmitButton.tsx
@@ -1,19 +1,24 @@
 import styled from 'styled-components';
+import { MoonLoader } from 'react-spinners';
 
 interface SubmitButtonProps {
   content: string;
   isvalid: boolean;
+  isLoading?: boolean;
 }
 
-const SubmitButton = ({ content, isvalid }: SubmitButtonProps) => {
+const SubmitButton = ({ content, isvalid, isLoading }: SubmitButtonProps) => {
   return (
-    <Button type="submit" isvalid={isvalid}>
-      {content}
+    <Button type="submit" isvalid={isvalid} disabled={isLoading}>
+      {isLoading ? <MoonLoader color='black' size="20px" /> : content}
     </Button>
   );
 };
 const Button = styled.button<{ isvalid: boolean }>`
   align-self: flex-end;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-radius: 10px;
   width: 35.8vw;
   height: 4.7vh;
