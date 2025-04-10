ac54bd7176d815f2ae3b3301d8efe245babebaaf
Tnalxmsk <alstnwkd990@naver.com>
2025-04-10 23:38:36 +0900

    :sparkles: feat: 날씨, 상황 페이지 index.ts 추가

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -3,3 +3,5 @@ export { BodySurveyPage } from '@home/ui/BodySurveyPage';
 export { BodyTypePage } from '@home/ui/BodyTypePage';
 export { StyleSurveyPage } from '@home/ui/StyleSurveyPage';
 export { RecommendationResultPage } from '@home/ui/RecommendationResultPage.tsx';
+export { WeatherStyleSurveyPage } from '@home/ui/WeatherStyleSurveyPage.tsx';
+export { SituationStyleSurveyPage } from '@home/ui/SituationStyleSurveyPage'
